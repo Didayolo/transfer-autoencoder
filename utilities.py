1f9b841c71e069c90aafcb70222d5f020d6441dc
Adrien Pavao <adrien.pavao@gmail.com>
2019-03-03 00:53:25 +0100

    Batch size increase, clean code.

--- utilities.py
+++ utilities.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def input_fn(dataset, perform_shuffle=False, batch_size=1):
     
     # maybe?
@@ -12,3 +14,10 @@ def input_fn(dataset, perform_shuffle=False, batch_size=1):
     iterator = dataset.make_one_shot_iterator()
     batch_features, batch_labels = iterator.get_next()
     return batch_features, batch_labels
+    
+def show_example(tensor_4d, labels=None, n=0, shape=(28,28)):
+    input_shape = tensor_4d[n].shape
+    print(input_shape)
+    if labels is not None:
+        print(labels[n])
+    plt.imshow(tensor_4d[n].reshape(shape))
